002fce8a48062d73943936e87f9041f463bcf846
Vedant <mahanavarvedant7049@gmail.com>
2024-04-18 22:42:23 +0530

    accounts app done for now

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
 ]
 
+AUTH_USER_MODEL = "accounts.CustomUser"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
